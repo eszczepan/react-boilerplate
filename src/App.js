c35120bc0343f8a54b2324b56c91cbabcaa7d958
Szczepan <s.szablej1@gmail.com>
2020-11-18 18:01:07 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MainTemplate from './templates/MainTemplate';
+
+const App = () => (
+  <MainTemplate>
+    <h1>Hello World</h1>
+  </MainTemplate>
+);
+
+export default App;
---
+++ src/theme/mainTheme.js
@@ -0,0 +1,17 @@
+export const theme = {
+  primary: '#00D563',
+  grey: '#FAFAFA',
+  dark: '#1C1D23',
+  light: 300,
+  semiBold: 600,
+  bold: 700,
+  fontSize: {
+    xxs: '1rem',
+    xs: '1.2rem',
+    s: '1.4rem',
+    m: '1.8rem',
+    l: '2rem',
+    xl: '2.4rem',
+    xxl: '4rem',
+  },
+};
